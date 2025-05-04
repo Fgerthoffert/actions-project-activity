195015fffd903ffcf01fb01610a3b30379a458cf
Francois G. <fgerthoffert@jahia.com>
2025-05-04 20:05:57 +0200

    Trying to fix path issue

--- src/views/index.ts
+++ src/views/index.ts
@@ -4,8 +4,10 @@ import path from 'path'
 import { MetricGroup } from '../types/index.js'
 
 import { getOutputDirectory } from './getOutputDirectory.js'
-import { loadHTMLTemplate } from './loadHTMLTemplate.js'
 import { writeHTMLTemplate } from './writeHTMLTemplate.js'
+
+import { tpl as dashboardTemplate } from './compiled-templates/group-view.html.base64.js'
+import { tpl as indexTemplate } from './compiled-templates/index.html.base64.js'
 
 export const buildViews = async ({
   inputViewsOutputPath,
@@ -16,18 +18,16 @@ export const buildViews = async ({
 }): Promise<null> => {
   const outputDir = await getOutputDirectory(inputViewsOutputPath)
 
-  const htmlTemplatesPath = path.join(process.cwd(), 'dist', 'templates')
-  const htmlDashboardTemplate = path.join(htmlTemplatesPath, 'group-view.html')
-
-  const htmlTemplate = await loadHTMLTemplate(htmlDashboardTemplate)
-
   const indexRows = []
   for (const group of groups) {
     core.info(`Building view for group ${group.name}`)
     const base64String = await Buffer.from(JSON.stringify(group)).toString(
       'base64'
     )
-    const updatedHtmlTemplate = htmlTemplate.replace('REPLACE_ME', base64String)
+    const updatedHtmlTemplate = dashboardTemplate.replace(
+      'REPLACE_ME',
+      base64String
+    )
     await writeHTMLTemplate(
       path.join(outputDir, `${group.id}.html`),
       updatedHtmlTemplate
@@ -40,12 +40,13 @@ export const buildViews = async ({
   }
 
   // Create an index.html file listing all dashboards
-  const htmlIndexTemplatePath = path.join(htmlTemplatesPath, 'index.html')
-  const htmlIndex = await loadHTMLTemplate(htmlIndexTemplatePath)
   const base64StringIndex = await Buffer.from(
     JSON.stringify(indexRows)
   ).toString('base64')
-  const updatedHtmlIndex = htmlIndex.replace('REPLACE_ME', base64StringIndex)
+  const updatedHtmlIndex = indexTemplate.replace(
+    'REPLACE_ME',
+    base64StringIndex
+  )
   await writeHTMLTemplate(path.join(outputDir, 'index.html'), updatedHtmlIndex)
 
   return null
