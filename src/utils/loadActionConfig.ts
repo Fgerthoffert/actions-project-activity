8d12f401ade9af3b32ce532129393a025d877e4b
Francois G. <fgerthoffert@jahia.com>
2025-05-07 07:15:35 -0400

    feat: Introduced stream template and fixed base64 decoding issue (#3)

--- src/utils/loadActionConfig.ts
+++ src/utils/loadActionConfig.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import fs from 'fs'
 import YAML from 'yaml'
 
-import { Config } from '../types/index.js'
+import { Config, ConfigGroup, ConfigTemplate } from '../types/index.js'
 
 // Loads a config YAML file from the provided path
 export const loadActionConfig = async (
@@ -13,5 +13,27 @@ export const loadActionConfig = async (
   core.info(`Loaded action config file at: ${inputYamlConfig}`)
   core.debug('JSON representation of the YAML config:')
   core.debug(JSON.stringify(config))
-  return config
+
+  // The template itself might contain templates, which are useful to simplify the configuration
+
+  return {
+    ...config,
+    groups: config.groups.map((group: ConfigGroup) => {
+      let streams = group.streams
+      if (group.streamsTemplate !== undefined) {
+        // If a template is defined, we need to find the corresponding template
+        // and replace the streams with the template
+        const streamTemplate = config.templates.find(
+          (template: ConfigTemplate) => template.name === group.streamsTemplate
+        )
+        if (streamTemplate !== undefined) {
+          streams = streamTemplate.streams
+        }
+      }
+      return {
+        ...group,
+        streams: streams
+      }
+    })
+  }
 }
